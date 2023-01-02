cd1526eb0d93ea79bf53cebcbe63d4a0c062cf55
Collin Stasiak <collinstasiak@gmail.com>
2023-01-01 22:50:04 -0500

    Began Structuring for Reverb

--- AudioTools/Program.cs
+++ AudioTools/Program.cs
@@ -10,7 +10,7 @@ namespace AudioTools
         {
             Console.WriteLine("Hello World!");
             AudioFileHandler Test1 = new();
-            Console.WriteLine($"Left:  {Test1.Left}");
+            Console.WriteLine($"Left: {Test1.Left}");
             Console.WriteLine($"Right: {Test1.Right}");
         }
     }
--- AudioTools/Reverberator.cs
+++ AudioTools/Reverberator.cs
@@ -5,13 +5,13 @@ namespace AudioTools
     {
 
 
-        public float[] CombFilter()
+        public float[] CombFilter(float[] samples, int samplelength, float delay, float decay, float samplerate)
         {
             float[] taco = new float[1];
             return taco;
         }
 
-        public float[] AllPassFilter() {
+        public float[] AllPassFilter(float[] sample, int sampleslength, float samplerate) {
             float[] taco = new float[1];
             return taco;
         }
