ddf1c98dc62b3c50ecbfcd49171b38d59b034e5c
Collin Stasiak <collinstasiak@gmail.com>
2023-01-12 23:27:13 -0500

    Adding EQ Pedal and Chorus pedal

---
+++ AudioTools/EditingTools/BasicEQPedal.cs
@@ -0,0 +1,124 @@
+﻿using AudioTools.AudioFileTools;
+
+namespace AudioTools.EditingTools
+{
+	public class ButterWorthFilterEQ : IPedal
+	{
+        public IAudioData AudioFile { get; set; }
+        public Dictionary<string, float> Parameters { get; set; }
+        public bool IsEnabled { get; set; }
+        public float LowPassCutOff
+        {
+            get { return Parameters["LowPassCutOff"]; }
+            set { Parameters["LowPassCutOff"] = value; }
+        }
+        public float HighPassCutOff
+        {
+            get { return Parameters["HighPassFilter"]; }
+            set { Parameters["HighPassFilter"] = value; }
+        }
+
+        public ButterWorthFilterEQ(IAudioData audioData, float lowPassCutOff, float highPassCutOff, float rate)
+        {
+            AudioFile = audioData;
+            Parameters = new Dictionary<string, float>
+            {
+                {"MixPercent",  lowPassCutOff },
+                {"Depth", highPassCutOff }
+            };
+            HighPassCutOff = highPassCutOff;
+            LowPassCutOff = lowPassCutOff;
+        }
+        //The buttersworth function is used to roll high end off audio
+        //The order parameter referrs to how many 'poles' the function will have
+        //Higher order filters will be more computationally intense but they will have steeper roll off
+        //Differential equation for buttersworth filter is
+        //y[n] = b[0]*x[n] + b[1]*x[n-1] + ... + b[M]*x[n-M] - a[1]*y[n-1] - ... - a[N]*y[n-N]
+        public float[] ButtersworthLowPassFilter(int order)
+        {
+            float[] output = new float[AudioFile.Samples.Length];
+            float[] coeffcientA = new float[order + 1];
+            float[] coeffcientB = new float[order + 1];
+            ComputeButtersWorthLowFilterCoeffcients(order, ref coeffcientA, ref coeffcientB);
+            for (int n = 0; n < AudioFile.Samples.Length; n++)
+            {
+                float y = 0;
+                for (int i = 0; i <= order; i++)
+                {
+                    if (n - i >= 0)
+                        y += coeffcientB[i] * AudioFile.Samples[n - i] - coeffcientA[i] * output[n - i];
+                }
+                output[n] = y;
+            }
+            return output;
+        }
+
+        public void ComputeButtersWorthLowFilterCoeffcients(int order,
+            ref float[] coeffcientA, ref float[] coeffcientB)
+        {
+            for (int i = 0; i < order; i++)
+            {
+                //This is another part of Butterworth equation that normalizes audio roll the audio off
+                //The volume of the audio will get lower the closer we get to the cutoff
+                float normilizationFactor = (float)Math.Pow((2 * LowPassCutOff / AudioFile.SampleRate), i);
+                coeffcientA[i] = normilizationFactor / Factorial(i);
+                coeffcientB[i] = normilizationFactor / Factorial(i + 1);
+            }
+        }
+
+        public static int Factorial(int input)
+        {
+            int output = 1;
+            for (int i = 1; i <= input; i++)
+            {
+                output = output * i;
+            }
+            return output;
+        }
+
+        public float[] ButtersworthHighPassFilter(int order)
+        {
+            float[] output = new float[AudioFile.Samples.Length];
+            float[] coeffcientB = new float[order + 1];
+            float[] coeffcientA = new float[order + 1];
+            ComputeButtersWorthHighFilterCoeffcients(order, ref coeffcientB, ref coeffcientA);
+            for (int n = 0; n < AudioFile.Samples.Length; n++)
+            {
+                float y = 0;
+                for (int i = 0; i <= order; i++)
+                {
+                    if (n - i >= 0)
+                        y += coeffcientB[i] * AudioFile.Samples[n - i] - coeffcientA[i] * output[n - i];
+                }
+                output[n] = y;
+            }
+            return output;
+        }
+
+        public void ComputeButtersWorthHighFilterCoeffcients(int order,
+            ref float[] coeffcientA, ref float[] coeffcientB)
+        {
+            {
+                //Q in the transfer formula represents how fast the volume drops as we approach the cutoff
+                float Q = (float)(Math.Sqrt(Math.Pow(2, 1.0 / order) - 1) / (Math.Pow(2, 1.0 / order) - 1));
+                for (int i = 0; i <= order; i++)
+                {
+                    // This is another part of Butterworth equation that normalizes the audio roll-off
+                    // The volume of the audio will get lower the closer we get to the cutoff
+                    float normilizationFactor = (float)Math.Pow((2 * HighPassCutOff / AudioFile.SampleRate), i);
+                    coeffcientB[i] = normilizationFactor;
+                    if (i == 0)
+                        coeffcientA[i] = 1;
+                    else if (i == 1)
+                        coeffcientA[i] = Q;
+                    else
+                        coeffcientA[i] = 0;
+                }
+            }
+        }
+        public void ApplyEffect()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AudioTools/EditingTools/ChorusPedal.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using AudioTools.AudioFileTools;
+
+/*
+ * Start by generating a modulating signal, which is often a sine wave that is 
+ * slightly detuned from the original signal. The frequency of the modulating 
+ * signal is controlled by the rate parameter, and the amount of detuning is 
+ * controlled by the depth parameter.
+Mix the modulating signal with the dry signal to create the modulated signal.
+This is done by multiplying the dry signal with the modulating signal at each sample.
+Apply a low-pass filter to the modulated signal to remove any high-frequency
+content that was added during the modulation process. This helps to reduce the
+harshness of the effect and make it sound more natural.
+Use an envelope generator to control the amount of modulation applied to the
+signal over time. The envelope generator is used to create a smooth transition
+between the modulated and dry signal, this can be done by using an envelope
+generator to modulate the gain of the modulated signal.
+Mix the dry signal and the modulated signal together to create the final chorus
+effect. The mix parameter controls the balance between the dry and modulated signals.
+Add the final output to the output buffer.
+The exact implementation of the algorithm will depend on the programming */
+
+namespace AudioTools.EditingTools
+{
+    public class ChorusPedal : IPedal
+    {
+        public ChorusPedal(IAudioData audioData, float mixPercent, float depth, float rate)
+        {
+            AudioFile = audioData;
+            Parameters = new Dictionary<string, float>
+            {
+                {"MixPercent",  mixPercent },
+                {"Depth", depth },
+                {"Rate", rate }
+            };
+            MixPercent = mixPercent;
+            Depth = depth;
+            Rate = rate;
+        }
+        public IAudioData AudioFile { get; set; }
+        public Dictionary<string, float> Parameters { get; set; }
+        public bool IsEnabled { get; set; }
+        public float MixPercent
+        {
+            get { return Parameters["MixPercent"]; }
+            set { Parameters["MixPercent"] = value; }
+        }
+        public float Depth
+        {
+            get { return Parameters["Depth"]; }
+            set { Parameters["Depth"] = value; }
+        }
+        public float Rate
+        {
+            get { return Parameters["Rate"]; }
+            set { Parameters["Rate"] = value; }
+        }
+
+        //Combine the dry and wet signal at specified rate by Mixpercent
+        public void ApplyEffect()
+        {
+            var output = new float[AudioFile.Samples.Length];
+            output = ModulateSignal(AudioFile.Samples);
+            output = CombineWetDry(output);
+
+        }
+        public float[] CombineWetDry(float[] wetInput)
+        {
+            var mixAudio = new float[AudioFile.Samples.Length];
+            for (int i = 0; i < AudioFile.Samples.Length; i++)
+                mixAudio[i] = (100 - MixPercent) * AudioFile.Samples[i] + MixPercent * wetInput[i];
+            return mixAudio;
+        }
+        /* we're gonna use this formula for sin wave modulation y(t) = A * sin(2 * pi * f * t + phi)
+         * Where y(t) is an element of the audio sample array
+         * A is the max amplitude represented by the max value possible in int32
+         * f is the frequency, To calculate the frequency value you need to use this formula: f = 1 / (sample rate / desired frequency(we call it rate)
+         * T is the time measured in seconds so we can calculate this pretty easily by, t = sample index / sample rate
+         * phi is not used in this instance so we'll just set it to 0
+         * */
+        
+        public float[] ModulateSignal(float[] input)
+        {
+            var output = new float[AudioFile.Samples.Length];
+            float amplitude = float.MaxValue;
+            float frequency = 1 / (AudioFile.SampleRate / Rate);
+            for(int i=0; i< AudioFile.Samples.Length; i++)
+            {
+                output[i] = (float)(amplitude * Math.Sin(2 * Math.PI * frequency * (i / AudioFile.SampleRate) + 0 ));
+            }
+            return output;
+        }
+    }
+}
+
