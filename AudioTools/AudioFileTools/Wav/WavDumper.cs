48bd1a3ad237fc0e3aea796cabde8828137a210a
Collin Stasiak <collinstasiak@gmail.com>
2023-01-15 22:32:43 -0500

    Broke out a method that was doing more than one thing into two

--- AudioTools/AudioFileTools/Wav/WavDumper.cs
+++ AudioTools/AudioFileTools/Wav/WavDumper.cs
@@ -2,37 +2,42 @@
 {
     public static class WavDumper
     {
-        public static bool PackSamplesToWav(WavData WavObject, string fileNameOut)
+        public static bool PackSamplesToWav(WavData wavObject, string fileNameOut)
         {
-            byte[] wavAsBytes = new byte[8 + WavObject.HeaderData["fileSize"]];
+            if(SampleArrayToBytes(wavObject, out byte[] wavAsBytes) != true)
+            { return false; }
+            File.WriteAllBytes(fileNameOut, wavAsBytes);
+            return true;
+
+        }
+        public static bool SampleArrayToBytes(WavData wavObject, out byte[] wavAsBytes)
+        {
+            wavAsBytes = new byte[8 + wavObject.HeaderData["fileSize"]];
             //Put Header in bytearray first
-            Buffer.BlockCopy(WavObject.RawHeader, 0, wavAsBytes, 0, WavObject.RawHeader.Length);
+            Buffer.BlockCopy(wavObject.RawHeader, 0, wavAsBytes, 0, wavObject.RawHeader.Length);
             //Switch to handle re-encoding at differnet bitrates
-            switch (WavObject.HeaderData["bitDepth"])
+            switch (wavObject.HeaderData["bitDepth"]) //Convert File to original bit size and return true if implementation for that bit-rate exists
             {
                 case 16:
                     {
                         //Convert Float(32 bits) back to Int16s
-                        short[] shortSamples = new short[WavObject.Samples.Length];
-                        for (int i = 0; i < WavObject.Samples.Length; i++)
+                        short[] shortSamples = new short[wavObject.Samples.Length];
+                        for (int i = 0; i < wavObject.Samples.Length; i++)
                         {
-                            shortSamples[i] = (short)Math.Floor(WavObject.Samples[i] * 32767);
+                            shortSamples[i] = (short)Math.Floor(wavObject.Samples[i] * 32767);
                         }
-                        Buffer.BlockCopy(shortSamples, 0, wavAsBytes, WavObject.RawHeader.Length, WavObject.HeaderData["bytes"]);
-                        Console.WriteLine("CONVETED TIME TO WRITE");
-                        //Write byte array to file
-                        File.WriteAllBytes(fileNameOut, wavAsBytes);
+                        Buffer.BlockCopy(shortSamples, 0, wavAsBytes, wavObject.RawHeader.Length, wavObject.HeaderData["bytes"]);
                         return true;
                     }
                 case 32:
                     {
-                        Buffer.BlockCopy(WavObject.Samples, 0, wavAsBytes, WavObject.RawHeader.Length, WavObject.HeaderData["bytes"]);
+                        Buffer.BlockCopy(wavObject.Samples, 0, wavAsBytes, wavObject.RawHeader.Length, wavObject.HeaderData["bytes"]);
                         return true;
                     }
                 case 64:
                     {
-                        double[] sampleAsDouble = Array.ConvertAll(WavObject.Samples, e => (double)e);
-                        Buffer.BlockCopy(WavObject.Samples, 0, wavAsBytes, WavObject.RawHeader.Length, WavObject.HeaderData["bytes"]);
+                        double[] sampleAsDouble = Array.ConvertAll(wavObject.Samples, e => (double)e);
+                        Buffer.BlockCopy(wavObject.Samples, 0, wavAsBytes, wavObject.RawHeader.Length, wavObject.HeaderData["bytes"]);
                         return true;
                     }
 
